2311f89689e9f4a702d134cad575e8bc9961516a
Megan Pera <mpera@castlighthealth.com>
2019-08-06 19:50:21 -0700

    adding build folder to git

---
+++ build/precache-manifest.f3edc94048f7988a1c1c02457d5224ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33356021c5842e4a239f494366146ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d347c1d2386245271c6",
+    "url": "/static/css/2.41a373cd.chunk.css"
+  },
+  {
+    "revision": "049a45a5a84344932d29",
+    "url": "/static/css/main.259ef5ca.chunk.css"
+  },
+  {
+    "revision": "3d347c1d2386245271c6",
+    "url": "/static/js/2.65af7f0c.chunk.js"
+  },
+  {
+    "revision": "049a45a5a84344932d29",
+    "url": "/static/js/main.9961982c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
